7080cc4115650607531d5724429e11d0a118f791
Metodi Velev <metodi.velev@yahoo.com>
2023-01-30 22:18:38 +0200

    Initial commit

---
+++ src/main/java/com/example/newsservice/Newsservice.java
@@ -0,0 +1,13 @@
+package com.example.newsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Newsservice {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Newsservice.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/newsservice/NewsserviceTests.java
@@ -0,0 +1,13 @@
+package com.example.newsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsserviceTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
