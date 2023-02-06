205a7b8455eacb7b4703677d30070f25730a801e
Metodi Velev <metodi.velev@yahoo.com>
2023-02-06 19:30:14 +0200

    Add Exception handler for unauthorized resource access.

--- src/main/java/com/example/newsservice/exception/ExceptionHandling.java
+++ src/main/java/com/example/newsservice/exception/ExceptionHandling.java
@@ -4,10 +4,13 @@ import com.example.newsservice.dto.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
@@ -24,6 +27,13 @@ public class ExceptionHandling implements ErrorController {
     public ResponseEntity<HttpResponse> internalServerErrorException(Exception exception) {
         LOGGER.error(exception.getMessage());
         return createHttpResponse(INTERNAL_SERVER_ERROR, INTERNAL_SERVER_ERROR_MSG);
+    }
+
+    @ExceptionHandler({AccessDeniedException.class})
+    public ResponseEntity<Object> handleAccessDeniedException(
+            Exception ex, WebRequest request) {
+        return new ResponseEntity<Object>(
+                "Access denied. User is unauthorized to access this resource.", new HttpHeaders(), HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(NotAnImageFileException.class)
